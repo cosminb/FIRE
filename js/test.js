f54ed415085248d123b783586e0973fc2e3a688e
Cosmin Branescu <cosmin.branescu@aciworldwide.com>
2016-11-17 14:25:55 +0200

    clean timeline

--- js/test.js
+++ js/test.js
@@ -6,8 +6,8 @@ app.test = {
 		var pl = this.pl[ p];
 		
 		do { 
-			var dx = Math.round( Math.random() * 4 ) -4 ;
-			var dz = Math.round( Math.random() * 4 ) -4 ;
+			var dx = Math.round( Math.random() * 10 ) -5 ;
+			var dz = Math.round( Math.random() * 10 ) -5 ;
 		} while ( (dx == 0 && dz == 0) || (dx!=0 && dz!=0)  );
 		
 		pl.xxx +=  dx;
@@ -31,14 +31,8 @@ app.test = {
 			this.pl[ i ] = { xxx : Math.round( Math.random( ) * 50 ) , zzz : Math.round( Math.random( ) * 50 ) }
 		}
 	},
-	
-	xnext : function ( ) {
-        
-		for ( var i = 0; i<35; i++ ) 
-			this.movePlayer( i );
-	},
-    
-    anext : function () {
+
+    next : function () {
         
       app.timeline.addScene();
 
@@ -53,71 +47,14 @@ app.test = {
 	
 	add : function ( ) {
 		for( var i = 0 ; i < 50; i++  ){
-			this.anext()
+			this.next()
 		}	
-	},
-	
-	next : function () {
-		
-		app.timeline.addScene();
-		
-		for ( var i = 0; i< 35;i++ ) {
-			var pos = this.movePlayer( i );
-			app.api.moveTo( i, pos.x, pos.z );
-			//pos = pos3d( pos.x, pos.z );
-			
-			//app.timeline.addToScene( i, pos )
-		}      
 	},
 	
 	auto : function ( ) {
 		this.add();
-		window.setInterval ( function ( ) {
-		
-			app.test.next( i );
-		}, 500);
 	}
 }
 
-
-
-/*
-
-var xfn = function () {
-    var overlay, lastCount, lastTime, timeoutFun;
-
-    overlay = document.createElement('div');
-    overlay.style.background = 'rgba(0, 0, 0, .7)';
-    overlay.style.bottom = '0';
-    overlay.style.color = '#fff';
-    overlay.style.display = 'inline-block';
-    overlay.style.fontFamily = 'Arial';
-    overlay.style.fontSize = '10px';
-    overlay.style.lineHeight = '12px';
-    overlay.style.padding = '5px 8px';
-    overlay.style.position = 'fixed';
-    overlay.style.right = '0';
-    overlay.style.zIndex = '1000000';
-    overlay.innerHTML = 'FPS: -';
-    document.body.appendChild(overlay);
-
-    lastCount = window.mozPaintCount;
-    lastTime = performance.now();
-
-    timeoutFun = function () {
-        var curCount, curTime;
-
-        curCount = window.mozPaintCount;
-        curTime = performance.now();
-        overlay.innerHTML = 'FPS: ' + ((curCount - lastCount) / (curTime - lastTime) * 1000).toFixed(2);
-        lastCount = curCount;
-        lastTime = curTime;
-        setTimeout(timeoutFun, 1000);
-    };
-
-    setTimeout(timeoutFun, 1000);
-}
-
-xfn();*/
 
 
--- js/timeline.js
+++ js/timeline.js
@@ -27,8 +27,6 @@ app.timeline = {
     currentScene : null,
     
     initCurrentScene : function ( ) {         
-         //this.currentScene = this.screenPlay.shift();        
-
          var scene = { items : [] };
          this.currentScene = scene;
          
@@ -72,14 +70,9 @@ app.timeline = {
          this.nrSteps = 0;
     },
     
-    runStep   : function ( ) {
-        
-    },
-    
-    
     nrSteps : 1000,
     
-    _runAnimation : function ( ) {
+    runAnimations : function ( ) {
         
            var step = this.nrSteps++;
            
@@ -105,65 +98,7 @@ app.timeline = {
           }
     },
 	
-	getTime : function () {
-		return new Date().getTime();
-	},
-	runAnimations : function ( ) {
-		return this._runAnimation(); 
-		var time = this.getTime(); 
-		
-		for ( var i = 0; i< this.animations.length; i++ ) {
-			
-			var end = this.runAnimationStep( this.animations[ i ] , time);
-			
-			if ( end ) {
-				this.animations.splice( i, 1 );
-				i--;	
-			}
-		}
-		
-	},
-    
-	add : function ( name , obj, parameters ) {
-		
-		if ( this.namedAnimations[ name ] ) {
-			var animation = this.namedAnimations[name];
-			
-			animation.addKeyFrame( parameters );
-			
-			if ( animation.isSleeping ) {
-				animation.nextKeyFrame();
-				this.animations.push( animation ) ;
-			}
-			return;
-		}
-		
-		else { 
-			var animation = new app.animation( obj, parameters );
-			this.animations.push( animation );
-			this.namedAnimations[name] = animation;
-		}
-	},
 	
-	runAnimationStep : function ( animation , time ) {
-		
-		var elapsed = time - animation.start;
-		var step    = elapsed / animation.duration
-		
-		
-		var values  = animation.step( step, elapsed );
-		var obj     = animation.obj;
-		
-		
-		for ( var i in animation.currentPosition ) {
-			obj.position[i] = animation.currentPosition[i]
-		}
-		
-		if ( time > animation.end ) {
-			
-			return animation.nextKeyFrame();
-		}
-	}
 }
 
 
@@ -219,69 +154,5 @@ ani.jump = {
 	},
 
 	
-}
-
-
-app.animation = function ( obj, parameters ) {
-	
-	this.frames = [];
-	
-	this.duration = parameters.duration;
-	
-	this.obj   = obj.obj;
-	
-	this.endPosition  = parameters.endPosition;
-	this.startPosition = { x : this.obj.position.x, z : this.obj.position.z };
-	
-	this.currentPosition = { x : 0, z : 0 };
-	
-	this.deltaPosition = { x : 0, z : 0 };
-	
-	this.start = app.timeline.getTime();
-	this.end   = this.start + this.duration;
-	
-	
-	this.computeDeltaPosition();
-	
-	
-}
-
-app.animation.prototype.computeDeltaPosition = function ( ) {
-	this.deltaPosition.x =  this.endPosition.x - this.startPosition.x;
-	this.deltaPosition.z = this.endPosition.z - this.startPosition.z;
-}
-
-app.animation.prototype.step = function ( time ) {
-	
-	this.currentPosition.x = this.startPosition.x + this.deltaPosition.x * time;
-	this.currentPosition.z = this.startPosition.z + this.deltaPosition.z * time;
-	
-	//console.log( this.currentPosition );
-}
-
-app.animation.prototype.addKeyFrame = function( parameters ) {
-	this.frames.push( parameters );
-}
-
-app.animation.prototype.nextKeyFrame = function () {
-	if (this.frames.length ) {
-		
-		var frame = this.frames.shift();
-		
-		this.endPosition.x = frame.endPosition.x;
-		this.endPosition.z = frame.endPosition.z;
-		
-		
-		this.startPosition.x = this.currentPosition.x;
-		this.startPosition.z = this.currentPosition.z;
-
-		this.start = app.timeline.getTime(); 
-		this.end   = this.start + this.duration;
-		this.computeDeltaPosition();
-	}
-	else {
-		this.isSleeping = true;
-		return true;
-	}
 }
 
